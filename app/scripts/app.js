f6479a95d4718ff0a04972ac7c857e0951f56ef0
Robert Ovens <robert.ovens@gmail.com>
2015-04-19 02:42:10 +0000

    another

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fooApp
+ * @description
+ * # fooApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fooApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
